53777d52ec0941b8ef0f492d340a2231dc582d95
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-08 22:41:13 -0800

    map function to generate buttons

---
+++ src/ButtonList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class ButtonList extends React.Component {
+    render() {
+        return (
+                <li><img src={this.props.src}alt="fb"/></li>
+            
+        )
+    }
+}
