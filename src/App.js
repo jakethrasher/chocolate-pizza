1a99b153db7fa9d351f25785982adcd7c2ede030
Lori Winston <Lcwinston@gmail.com>
2021-02-08 16:38:53 -0800

    recipe added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import './App.css';
 import React from 'react';
 import WonderfulHeader from './header';
+import Image from './image';
+import Recipe from './recipe';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Chocolate Pizza</h1>
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default class App extends React.Component{
   render (){
     return(
-      <div>
-      <WonderfulHeader/>
+    <div>
+        <WonderfulHeader/>
+        <Image/>
+        <Recipe/>
     </div>
     )
    
