7acbd12804b750ec4cb3c2b8532558e47054ec26
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-08 19:36:04 -0800

    ingredient list in progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import WonderfulHeader from './header';
 import Image from './image';
-import IngredientList from './recipe';
+import IngredientList from './IngredientList';
 
 
 export default class App extends React.Component{
