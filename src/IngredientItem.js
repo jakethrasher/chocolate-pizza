a727880960bc0ff15e6157e6f9f6ac5acee84043
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-08 21:23:10 -0800

    mapping data to render list

--- src/IngredientItem.js
+++ src/IngredientItem.js
@@ -3,13 +3,11 @@ import React from 'react';
 export default class IngredientItem extends React.Component{
     render(){
         return (
-            <div>
-                <label>
-                <input type="checkbox"/>
-                {this.props.amount}{this.props.name}
-                </label>
-            </div>
+           <li>
+               {this.props.amount}{this.props.name}
+           </li> 
         )
     }
 }
+
                 
--- src/IngredientList.js
+++ src/IngredientList.js
@@ -3,65 +3,58 @@ import IngredientItem from './IngredientItem';
 // import background from './public/list-bg.png'
 
 const ingredientArray= [
-    {
+    {   id:'1',
         amount:'1 1/2 cups',
         name: 'milk'
-    },{
+    },{ id:'2',
         amount: '1/2 cup',
         name:'mascarpone'
-    },{
+    },{ id:'4',
         amount: '1/2 tsp',
         name:'pink salt'
-    },{
+    },{ id:'5',
         amount: '1lb',
         name:'black mission figs'
-    },{
+    },{ id:'6',
         amount: '1/2 cup',
         name:'brown sugar'
-    },{
+    },{ id:'7',
         amount: '2 to 4 tbsp',
         name:'water'
-    },{
+    },{ id:'8',
         amount: '1/2 cup',
         name:'heavy cream'
-    },{
+    },{ id:'9',
         amount: '1/3 cup',
         name:'granulated sugar'
-    },{
+    },{ id:'10',
         amount: '2',
         name:'egg yolks'
-    },{
+    },{ id:'11',
         amount: '1',
         name:'lemon juiced'
-    },{
+    },{ id:'12',
         amount: '2 tbsp',
         name:'butter'
-    },{
+    },{ id:'13',
         amount: '1 cup',
         name:'Honey roasted pecans'
     }
 ]
-const componentArray = ingredientArray.map(ingredient => {
-    return(<IngredientItem amount={ingredient.amount} name={ingredient.name}/>)
-    
-})
-console.log(componentArray)
 
 export default class IngredientList extends React.Component {
     render() {
         return (
             <div className="ingredients" style={{ backgroundImage: 'url(./list-bg.png)' }}
             >
-               <IngredientItem amount="1 1/cups" name="milk"/>
-               <IngredientItem amount="1/2 cup" name="mascarpone"/>
-               <IngredientItem amount="1/2 tsp" name="pink salt"/>
-               <IngredientItem amount="1lb" name="black mission figs"/>
-               <IngredientItem amount="1/2 cup" name="brown sugar"/>
-               <IngredientItem amount="2 to 4 tbsp" name="water"/>
-               <IngredientItem amount="1 1/2 cup" name="heavy cream"/>
-
-
+                <ul>
+                {ingredientArray.map((item)=>(<IngredientItem  key={item.id} amount={item.amount} name = {item.name}/>)
+                )}
+                </ul>
             </div>
         )
     }
 }
+                    
+
+
